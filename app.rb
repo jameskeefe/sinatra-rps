5042c4217d44933694d100d85afb7ef65b2bfdbb
jameskeefe <204756923+jameskeefe@users.noreply.github.com>
2025-04-13 19:59:45 +0000

    additional changes to app.rb

--- app.rb
+++ app.rb
@@ -13,10 +13,13 @@ get("/rock") do
   # rock = 1, paper=2, scissors = 3
   if @rps==1
     @outcome = "tied"
+    @theyplayed = "rock"
   elsif @rps==2
     @outcome = "lost"
+    @theyplayed = "paper"
   else
     @outcome = "won"
+    @theyplayed = "scissors"
   end
   erb(:rock)
 end
@@ -26,10 +29,13 @@ get("/paper") do
   # rock = 1, paper=2, scissors = 3
   if @rps==1
     @outcome = "won"
+    @theyplayed ='rock'
   elsif @rps==2
     @outcome = "tied"
+    @theyplayed = 'paper'
   else
     @outcome = "lost"
+    @theyplayed = "scissors"
   end
   erb(:paper)
 end
@@ -39,10 +45,13 @@ get("/scissors") do
   # rock = 1, paper=2, scissors = 3
   if @rps==1
     @outcome = "lost"
+    @theyplayed ='rock'
   elsif @rps==2
     @outcome = "won"
+    @theyplayed = 'paper'
   else
     @outcome = "tied"
+    @theyplayed = "scissors"
   end
   erb(:scissors)
 end
