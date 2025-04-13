51080189b0ab2603d6d41804be1e358d351bc266
jameskeefe <204756923+jameskeefe@users.noreply.github.com>
2025-04-13 19:36:52 +0000

    first commit made /rock /paper and /scissor extensions and erb files

--- app.rb
+++ app.rb
@@ -3,7 +3,28 @@ require "sinatra/reloader"
 
 get("/") do
   "
-  <h1>Welcome to your Sinatra App!</h1>
+  <h1>Welcome to Rock Paper Scissors!</h1>
+  <p>Define some routes in app.rb</p>
+  "
+end
+
+get("/rock") do
+  "
+  <h1>Welcome to Rock Paper Scissors!</h1>
+  <p>Define some routes in app.rb</p>
+  "
+end
+
+get("/paper") do
+  "
+  <h1>Welcome to Rock Paper Scissors!</h1>
+  <p>Define some routes in app.rb</p>
+  "
+end
+
+get("/scissors") do
+  "
+  <h1>Welcome to Rock Paper Scissors!</h1>
   <p>Define some routes in app.rb</p>
   "
 end
