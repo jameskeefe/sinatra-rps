aacd7430ec7f66171daae0b19074d82684f2c542
jameskeefe <204756923+jameskeefe@users.noreply.github.com>
2025-04-13 19:55:55 +0000

    Finished changes for /rock /paper /scissors pages

--- app.rb
+++ app.rb
@@ -9,22 +9,40 @@ get("/") do
 end
 
 get("/rock") do
-  "
-  <h1>Welcome to Rock Paper Scissors!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  @rps = rand(1..3)
+  # rock = 1, paper=2, scissors = 3
+  if @rps==1
+    @outcome = "tied"
+  elsif @rps==2
+    @outcome = "lost"
+  else
+    @outcome = "won"
+  end
+  erb(:rock)
 end
 
 get("/paper") do
-  "
-  <h1>Welcome to Rock Paper Scissors!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  @rps = rand(1..3)
+  # rock = 1, paper=2, scissors = 3
+  if @rps==1
+    @outcome = "won"
+  elsif @rps==2
+    @outcome = "tied"
+  else
+    @outcome = "lost"
+  end
+  erb(:paper)
 end
 
 get("/scissors") do
-  "
-  <h1>Welcome to Rock Paper Scissors!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  @rps = rand(1..3)
+  # rock = 1, paper=2, scissors = 3
+  if @rps==1
+    @outcome = "lost"
+  elsif @rps==2
+    @outcome = "won"
+  else
+    @outcome = "tied"
+  end
+  erb(:scissors)
 end
